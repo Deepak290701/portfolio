d90739dfd73cb9d1966b872e02e6e2ea88ae02c3
Deepak290701 <deepaksharma290700@gmail.com>
2020-10-07 23:25:15 +0530

    added more projects in project section

--- js/app.js
+++ js/app.js
@@ -22,6 +22,6 @@ document.addEventListener('scroll',()=>{
 menu_item.forEach((item)=>{
     item.addEventListener('click',()=>{
         hamburger.classList.toggle('active');
-        mobile_menu.classList.toggle('active');
+        mobile_menu.classList.toggle('active'); 
     })
 })
